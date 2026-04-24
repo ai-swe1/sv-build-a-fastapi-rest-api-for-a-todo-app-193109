9ea870d95c07e8bde3443d53192352646664f9f5
Sajo Sam <66501284+sajosam@users.noreply.github.com>
2026-04-24 19:31:54 +0530

    feat: add seeds.py [3/15]

---
+++ seeds.py
@@ -0,0 +1,6 @@
+from models import Todo
+from datetime import datetime
+todos = [
+    Todo(title='Todo 1', description='Description 1', completed=False, created_at=datetime.now()),
+    Todo(title='Todo 2', description='Description 2', completed=False, created_at=datetime.now())
+]
